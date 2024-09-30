76c0039279275c957ac99b85d9fb4eaa9ee4ca4e
norrishuang <12380647@qq.com>
2024-09-30 15:12:48 +0800

    Initial commit

---
+++ test/emr-openldap-ranger-deploy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EmrOpenldapRangerDeploy from '../lib/emr-openldap-ranger-deploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/emr-openldap-ranger-deploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EmrOpenldapRangerDeploy.EmrOpenldapRangerDeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
